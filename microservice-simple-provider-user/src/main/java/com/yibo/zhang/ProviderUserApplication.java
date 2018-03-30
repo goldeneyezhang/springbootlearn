ec8bd0481348a31c964c9be6af42b19302f58d2f
yibozhang <yibozhang@ctrip.com>
2018-03-30 14:56:10 +0800

    eureka server cluster

--- microservice-simple-provider-user/src/main/java/com/yibo/zhang/ProviderUserApplication.java
+++ microservice-simple-provider-user/src/main/java/com/yibo/zhang/ProviderUserApplication.java
@@ -3,7 +3,9 @@ package com.yibo.zhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderUserApplication {
 	public static void main(String[] args) {
