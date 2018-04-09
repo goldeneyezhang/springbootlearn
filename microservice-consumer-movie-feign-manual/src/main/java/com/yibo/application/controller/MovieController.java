3fa17217c939ca6f2610386302bd3669320d44c7
yibozhang <yibozhang@ctrip.com>
2018-04-09 16:02:39 +0800

    增加movei-feign-manual

---
+++ microservice-consumer-movie-feign-manual/src/main/java/com/yibo/application/controller/MovieController.java
@@ -0,0 +1,61 @@
+package com.yibo.application.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.yibo.application.model.User;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+
+@RestController
+public class MovieController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	//@Autowired
+	//private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	@Value("${user.userServiceUrl}")
+	private String userServiceUrl;
+	
+	@GetMapping("/user/{id}")
+	public User findById(@PathVariable Integer id) {
+		System.out.println(userServiceUrl);
+		return this.restTemplate.getForObject("http://microservice-provider-user/"+id, User.class);
+	}
+	@GetMapping("/user2/{id}")
+	public User findByIdFeign(@PathVariable Integer id) {
+		return this.userFeignClient.findById(id);
+	}
+	//@GetMapping("/user-instance")
+	//public List<ServiceInstance> showInfo(){
+		//return this.discoveryClient.getInstances("microservice-provider-user");
+	//}
+	/**
+	 * 查询microservice-provider-user服务的信息并返回
+	 * @return
+	 */
+	@GetMapping("/log-instance")
+	public void logUserInstance(){
+		//return this.discoveryClient.getInstances("microservice-provider-user");
+		ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+		//打印当前选择的是哪个节点
+		MovieController.LOGGER.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
+	}
+}
---
+++ microservice-consumer-movie-feign-manual/src/main/java/com/yibo/application/model/User.java
@@ -0,0 +1,45 @@
+package com.yibo.application.model;
+
+import java.math.BigDecimal;
+
+public class User {
+
+	private Integer id;
+	private String username;
+	private String namecn;
+	private Integer age;
+	private BigDecimal balance;
+	
+	//getters and setters
+	public Integer getId() {
+		return this.id;
+	}
+	public void setId(Integer id) {
+		this.id=id;
+	}
+	public String getUsername() {
+		return this.username;
+	}
+	public void setUsername(String username) {
+		this.username=username;
+	}
+	public String getNamecn() {
+		return this.namecn;
+	}
+	public void setNamecn(String namecn) {
+		this.namecn=namecn;
+	}
+	public Integer getAge() {
+		return this.age;
+	}
+	public void setAge(Integer age) {
+		this.age=age;
+	}
+	public BigDecimal getBalance() {
+		return this.balance;
+	}
+	public void setBalance(BigDecimal balance){
+		this.balance=balance;
+	}
+	
+}
