15653843ae22e54e5aca2232781fae6c3f747a3e
yibozhang <yibozhang@ctrip.com>
2018-04-26 13:46:43 +0800

    zuul fallback

--- zuul/src/main/java/application/ZuulApplication.java
+++ zuul/src/main/java/application/ZuulApplication.java
@@ -3,8 +3,10 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
 import org.springframework.context.annotation.Bean;
 
+import application.fallback.UserFallbackProvider;
 import application.filter.PreRequestLogFilter;
 
 @EnableZuulProxy
@@ -13,6 +15,10 @@ public class ZuulApplication {
 	@Bean
 	public PreRequestLogFilter preRequestLogFilter() {
 		return new PreRequestLogFilter();
+	}
+	@Bean
+	public ZuulFallbackProvider zuulFallbackProvider() {
+		return new UserFallbackProvider();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
