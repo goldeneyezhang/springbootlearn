8012115e23347c795f50ffafd0a421f160b8cdf4
zyb张一博 <yibozhang@ctrip.com>
2018-07-11 16:02:37 +0800

    java8 predicate

--- Java8/src/main/java/javaeight/MethodReferenceStudy.java
+++ Java8/src/main/java/javaeight/MethodReferenceStudy.java
@@ -1,6 +1,7 @@
 package javaeight;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MethodReferenceStudy {
@@ -12,5 +13,12 @@ public class MethodReferenceStudy {
 		names.add("Taobao");
 		names.add("Yahoo");
 		names.forEach(System.out::println);
+		final Car car = Car.create( Car::new );
+		final List<Car> cars = Arrays.asList(car);
+		
+		cars.forEach(Car::collide);
+		cars.forEach(Car::repair);
+		final Car police = new Car();
+		cars.forEach(police::follow);
 	}
 }
