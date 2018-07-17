4603ea3e05b325d7c61d181b78a052003ee60fca
zyb张一博 <yibozhang@ctrip.com>
2018-07-17 09:31:37 +0800

    将sleuth日志写入elk

--- trace/src/main/java/com/yibo/simple_provider_user_trace/ProviderUserTraceElkApplication.java
+++ trace/src/main/java/com/yibo/simple_provider_user_trace/ProviderUserTraceElkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
-public class ProviderUserTraceApplication2 {
+public class ProviderUserTraceElkApplication {
 	 public static void main(String[] args) {
-		 SpringApplication.run(ProviderUserTraceApplication2.class, args);
+		 SpringApplication.run(ProviderUserTraceElkApplication.class, args);
 	 }
 }
